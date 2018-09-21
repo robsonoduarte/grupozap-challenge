fbe2a4e7478b0fe51d548155b57995330e78cff5
robsonoduarte <robson.o.d@gmail.com>
2018-09-20 22:55:21 -0300

    commit e push inicial do projeto.

---
+++ grupozap-challenge/src/main/java/br/com/grupozap/grupozapchallenge/GrupozapChallengeApplication.java
@@ -0,0 +1,12 @@
+package br.com.grupozap.grupozapchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupozapChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupozapChallengeApplication.class, args);
+	}
+}
