5044561b31ebe9bf57efeb898f065bd4b796ba9a
robsonoduarte <robson.o.d@gmail.com>
2018-09-25 14:31:24 -0300

    add find para imoveis legiveis para venda para o portal vivareal e imoveis para aluguel para o grupo zap.

--- grupozap-challenge/src/main/java/br/com/grupozap/challenge/repository/ImmobileRepository.java
+++ grupozap-challenge/src/main/java/br/com/grupozap/challenge/repository/ImmobileRepository.java
@@ -12,4 +12,10 @@ public interface ImmobileRepository extends MongoRepository<Immobile, String> {
 	@Query("{ $and: [{\"pricingInfos.businessType\" : \"RENTAL\"},{ \"pricingInfos.rentalTotalPrice\": { $lte: 4000 } }, { $where: \"this.pricingInfos.monthlyCondoFee  <= this.pricingInfos.rentalTotalPrice * 0.3\" }, {\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
 	Page<Immobile> findAllImobileToRentalForVivaReal(Pageable pageable);
 
+	@Query("{ $and: [{\"pricingInfos.businessType\" : \"SALE\"},{ \"pricingInfos.price\": { $lte: 700000 } }, {\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
+	Page<Immobile> findAllImobileToSaleForVivaReal(Pageable pageable);
+
+	@Query("{ $and: [{\"pricingInfos.businessType\" : \"RENTAL\"},{ \"pricingInfos.rentalTotalPrice\": { $gte: 3500 } }, {\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
+	Page<Immobile> findAllImobileToRentalForZap(Pageable pageable);
+
 }
--- grupozap-challenge/src/test/java/br/com/grupozap/challenge/repository/ImmobileRepositoryTest.java
+++ grupozap-challenge/src/test/java/br/com/grupozap/challenge/repository/ImmobileRepositoryTest.java
@@ -3,14 +3,12 @@ package br.com.grupozap.challenge.repository;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.assertThat;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,24 +23,13 @@ import br.com.grupozap.challenge.domain.Immobile;
 @RunWith(SpringRunner.class)
 public class ImmobileRepositoryTest {
 
+
 	@Autowired
 	private ImmobileRepository repository;
 
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-
-
+	// TESTES PARA FIND DE IMOVEIS LEGIVEIS PARA O VIVAREAL
 
 	@Test
 	public void shouldFindAllImobileThatIsEligibleToRentalForVivaReal() {
@@ -57,7 +44,7 @@ public class ImmobileRepositoryTest {
 	    //Um imóvel apenas é elegível pro portal Vivareal:
 	    //Quando for aluguel e no máximo o valor for de R$ 4.000,00.
 		//valor do condomínio não pode ser maior/igual que 30% do valor do aluguel.
-		// Ele tem lat e lon diferente a 0.
+		// Ele tem lat e lon diferente de 0.
 
 		pageResult.getContent().forEach( immobile -> {
 			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLat(), not(0.0));
@@ -65,6 +52,28 @@ public class ImmobileRepositoryTest {
 			assertThat(immobile.getPricingInfos().getBusinessType(), equalTo("RENTAL"));
 			assertThat(immobile.getPricingInfos().getRentalTotalPrice(), lessThanOrEqualTo(4000l));
 			assertThat(immobile.getPricingInfos().getMonthlyCondoFee(), lessThan((int) (immobile.getPricingInfos().getRentalTotalPrice() * 0.3)));
+		});
+
+	}
+
+
+	@Test
+	public void shouldFindAllImobileThatIsEligibleToSaleForVivaReal() {
+
+		Page<Immobile> pageResult =
+				repository.findAllImobileToSaleForVivaReal(PageRequest.of(0, 20));
+
+		assertThat(pageResult.getContent(), not(empty()));
+
+		//Um imóvel apenas é elegível pro portal Vivareal:
+		// Quando for venda e no máximo o valor for de R$ 700.000,00.
+		// Ele tem lat e lon diferente de 0.
+
+		pageResult.getContent().forEach( immobile -> {
+			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLat(), not(0.0));
+			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLon(), not(0.0));
+			assertThat(immobile.getPricingInfos().getBusinessType(), equalTo("SALE"));
+			assertThat(immobile.getPricingInfos().getRentalTotalPrice(), lessThanOrEqualTo(700000l));
 		});
 
 	}
@@ -92,7 +101,76 @@ public class ImmobileRepositoryTest {
 	}
 
 
+	@Test
+	public void shouldFindAllImobileThatIsEligibleToSAleForVivaRealAndReturnInPaginationMode() {
 
+		Page<Immobile> pageResult =
+				repository.findAllImobileToSaleForVivaReal(PageRequest.of(0, 20));
+
+		assertThat(pageResult.getContent(), hasSize(20));
+		assertThat(pageResult.getPageable().getPageNumber(), equalTo(0));
+		assertThat(pageResult.getPageable().getPageSize(), equalTo(20));
+		assertThat(pageResult.getTotalElements(), equalTo(4528l));
+
+		pageResult =
+				repository.findAllImobileToSaleForVivaReal(PageRequest.of(1, 20));
+
+		assertThat(pageResult.getContent(), hasSize(20));
+		assertThat(pageResult.getPageable().getPageNumber(), equalTo(1));
+		assertThat(pageResult.getPageable().getPageSize(), equalTo(20));
+		assertThat(pageResult.getTotalElements(), equalTo(4528l)); // TODO: remover ou adicionar comentário que esse número não pode mudar no arquivo...
+
+	}
+
+
+
+
+	// TESTES PARA FIND DE IMOVEIS LEGIVEIS PARA O ZAP
+
+	@Test
+	public void shouldFindAllImobileThatIsEligibleToRentalForVivaZap() {
+
+		Page<Immobile> pageResult =
+				repository.findAllImobileToRentalForZap(PageRequest.of(0, 20));
+
+
+		assertThat(pageResult.getContent(), not(empty()));
+
+	    //Um imóvel apenas é elegível pro portal Vivareal:
+	    //Quando for aluguel e no mínimo o valor for de R$ 3.500,00.
+		// Ele tem lat e lon diferente de 0.
+
+		pageResult.getContent().forEach( immobile -> {
+			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLat(), not(0.0));
+			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLon(), not(0.0));
+			assertThat(immobile.getPricingInfos().getBusinessType(), equalTo("RENTAL"));
+			assertThat(immobile.getPricingInfos().getRentalTotalPrice(), greaterThanOrEqualTo(3500l));
+		});
+
+	}
+
+
+
+	@Test
+	public void shouldFindAllImobileThatIsEligibleToRentalForZapAndReturnInPaginationMode() {
+
+		Page<Immobile> pageResult =
+				repository.findAllImobileToRentalForZap(PageRequest.of(0, 20));
+
+		assertThat(pageResult.getContent(), hasSize(20));
+		assertThat(pageResult.getPageable().getPageNumber(), equalTo(0));
+		assertThat(pageResult.getPageable().getPageSize(), equalTo(20));
+		assertThat(pageResult.getTotalElements(), equalTo(933L));
+
+		pageResult =
+				repository.findAllImobileToRentalForZap(PageRequest.of(1, 20));
+
+		assertThat(pageResult.getContent(), hasSize(20));
+		assertThat(pageResult.getPageable().getPageNumber(), equalTo(1));
+		assertThat(pageResult.getPageable().getPageSize(), equalTo(20));
+		assertThat(pageResult.getTotalElements(), equalTo(933L));
+
+	}
 
 
 
