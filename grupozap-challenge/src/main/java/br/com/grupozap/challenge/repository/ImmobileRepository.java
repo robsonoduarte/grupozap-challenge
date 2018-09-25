1c1bf6e7f0a7c702070e245c882d8d4437aeed79
robsonoduarte <robson.o.d@gmail.com>
2018-09-25 14:58:58 -0300

    finalizando o repo Immobile adicionando o find para os imoveis legiveis a venda para o portal zap.

--- grupozap-challenge/src/main/java/br/com/grupozap/challenge/repository/ImmobileRepository.java
+++ grupozap-challenge/src/main/java/br/com/grupozap/challenge/repository/ImmobileRepository.java
@@ -9,13 +9,16 @@ import br.com.grupozap.challenge.domain.Immobile;
 
 public interface ImmobileRepository extends MongoRepository<Immobile, String> {
 
-	@Query("{ $and: [{\"pricingInfos.businessType\" : \"RENTAL\"},{ \"pricingInfos.rentalTotalPrice\": { $lte: 4000 } }, { $where: \"this.pricingInfos.monthlyCondoFee  <= this.pricingInfos.rentalTotalPrice * 0.3\" }, {\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
+	@Query("{ $and: [{\"pricingInfos.businessType\" : \"RENTAL\"},{ \"pricingInfos.rentalTotalPrice\": { $lte: 4000 } },{ $where: \"this.pricingInfos.monthlyCondoFee  <= this.pricingInfos.rentalTotalPrice * 0.3\" },{\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
 	Page<Immobile> findAllImobileToRentalForVivaReal(Pageable pageable);
 
-	@Query("{ $and: [{\"pricingInfos.businessType\" : \"SALE\"},{ \"pricingInfos.price\": { $lte: 700000 } }, {\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
+	@Query("{ $and: [{\"pricingInfos.businessType\" : \"SALE\"},{ \"pricingInfos.price\": { $lte: 700000 } },{\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
 	Page<Immobile> findAllImobileToSaleForVivaReal(Pageable pageable);
 
-	@Query("{ $and: [{\"pricingInfos.businessType\" : \"RENTAL\"},{ \"pricingInfos.rentalTotalPrice\": { $gte: 3500 } }, {\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
+	@Query("{ $and: [{\"pricingInfos.businessType\" : \"RENTAL\"},{ \"pricingInfos.rentalTotalPrice\": { $gte: 3500 } },{\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
 	Page<Immobile> findAllImobileToRentalForZap(Pageable pageable);
+
+	@Query("{ $and: [{\"pricingInfos.businessType\" : \"SALE\"},{ \"pricingInfos.price\": { $gte: 600000 } },{ \"usableAreas\": { $gt: 3500 } },{\"address.geoLocation.precision\": {$ne : \"NO_GEOCODE\"}} ] }")
+	Page<Immobile> findAllImobileToSaleForZap(Pageable pageable);
 
 }
--- grupozap-challenge/src/test/java/br/com/grupozap/challenge/repository/ImmobileRepositoryTest.java
+++ grupozap-challenge/src/test/java/br/com/grupozap/challenge/repository/ImmobileRepositoryTest.java
@@ -3,6 +3,7 @@ package br.com.grupozap.challenge.repository;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.lessThan;
@@ -42,9 +43,9 @@ public class ImmobileRepositoryTest {
 
 
 	    //Um imóvel apenas é elegível pro portal Vivareal:
+		// Ele tem lat e lon diferente de 0.
 	    //Quando for aluguel e no máximo o valor for de R$ 4.000,00.
 		//valor do condomínio não pode ser maior/igual que 30% do valor do aluguel.
-		// Ele tem lat e lon diferente de 0.
 
 		pageResult.getContent().forEach( immobile -> {
 			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLat(), not(0.0));
@@ -66,8 +67,8 @@ public class ImmobileRepositoryTest {
 		assertThat(pageResult.getContent(), not(empty()));
 
 		//Um imóvel apenas é elegível pro portal Vivareal:
-		// Quando for venda e no máximo o valor for de R$ 700.000,00.
 		// Ele tem lat e lon diferente de 0.
+		// Quando for venda e no máximo o valor for de R$ 700.000,00.
 
 		pageResult.getContent().forEach( immobile -> {
 			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLat(), not(0.0));
@@ -136,15 +137,39 @@ public class ImmobileRepositoryTest {
 
 		assertThat(pageResult.getContent(), not(empty()));
 
-	    //Um imóvel apenas é elegível pro portal Vivareal:
-	    //Quando for aluguel e no mínimo o valor for de R$ 3.500,00.
+	    //Um imóvel apenas é elegível pro portal ZAP:
 		// Ele tem lat e lon diferente de 0.
+	    //Quando for aluguel e no mínimo o valor for de R$ 3.500,00.
 
 		pageResult.getContent().forEach( immobile -> {
 			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLat(), not(0.0));
 			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLon(), not(0.0));
 			assertThat(immobile.getPricingInfos().getBusinessType(), equalTo("RENTAL"));
 			assertThat(immobile.getPricingInfos().getRentalTotalPrice(), greaterThanOrEqualTo(3500l));
+		});
+
+	}
+
+	@Test
+	public void shouldFindAllImobileThatIsEligibleToSaleForVivaZap() {
+
+		Page<Immobile> pageResult =
+				repository.findAllImobileToSaleForZap(PageRequest.of(0, 20));
+
+
+		assertThat(pageResult.getContent(), not(empty()));
+
+		//Um imóvel apenas é elegível pro portal ZAP:
+		// Ele tem lat e lon diferente de 0.
+        // Quando for venda e no mínimo o valor for de R$ 600.000,00.
+		// O valor do metro quadrado (chave usableAreas) não pode ser menor/igual a R$ 3.500,00.
+
+		pageResult.getContent().forEach( immobile -> {
+			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLat(), not(0.0));
+			assertThat(immobile.getAddress().getGeoLocation().getLocation().getLon(), not(0.0));
+			assertThat(immobile.getPricingInfos().getBusinessType(), equalTo("SALE"));
+			assertThat(immobile.getPricingInfos().getPrice(), greaterThanOrEqualTo(600000l));
+			assertThat(immobile.getUsableAreas(), greaterThan(3500));
 		});
 
 	}
@@ -169,6 +194,28 @@ public class ImmobileRepositoryTest {
 		assertThat(pageResult.getPageable().getPageNumber(), equalTo(1));
 		assertThat(pageResult.getPageable().getPageSize(), equalTo(20));
 		assertThat(pageResult.getTotalElements(), equalTo(933L));
+
+	}
+
+
+	@Test
+	public void shouldFindAllImobileThatIsEligibleToSaleForZapAndReturnInPaginationMode() {
+
+		Page<Immobile> pageResult =
+				repository.findAllImobileToSaleForZap(PageRequest.of(0, 20));
+
+		assertThat(pageResult.getContent(), hasSize(12));
+		assertThat(pageResult.getPageable().getPageNumber(), equalTo(0));
+		assertThat(pageResult.getPageable().getPageSize(), equalTo(20));
+		assertThat(pageResult.getTotalElements(), equalTo(12L));
+
+		pageResult =
+				repository.findAllImobileToSaleForZap(PageRequest.of(1, 20));
+
+		assertThat(pageResult.getContent(), hasSize(0));
+		assertThat(pageResult.getPageable().getPageNumber(), equalTo(1));
+		assertThat(pageResult.getPageable().getPageSize(), equalTo(20));
+		assertThat(pageResult.getTotalElements(), equalTo(12L));
 
 	}
 
